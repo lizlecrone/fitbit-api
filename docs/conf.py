4a5f4cd772ea28943355c436b43b61ba8a2e02e3
lizlecrone <liz@looksharp.com>
2020-02-11 14:01:03 -0800

    attempt to refresh the token if token expired

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2020, Liz LeCrone'
 author = 'Liz LeCrone'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
